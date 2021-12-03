ed559b713b85df1d2dcf2003332a370e1a634222
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2021-12-03 21:30:23 +0530

    react-routing

---
+++ src/Routes/AllRoutes.jsx
@@ -0,0 +1,22 @@
+import { Route } from "react-router-dom";
+import About from "../pages/About";
+import Home from "../pages/Home";
+import Users from "../pages/Users";
+
+function AllRoutes() {
+  return (
+    <div>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/users">
+        <Users />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+    </div>
+  );
+}
+
+export default AllRoutes;
