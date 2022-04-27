d74a79e5f934f9ede5d51eccdaa1fe5b5a04a1d3
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2022-04-27 09:09:49 +0530

    new update

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,20 +1,20 @@
 import { Route } from "react-router-dom";
-import About from "../pages/About";
-import Home from "../pages/Home";
-import Users from "../pages/Users";
+// import About from "../pages/About";
+// import Home from "../pages/Home";
+// import Users from "../pages/Users";
 
 function AllRoutes() {
   return (
     <div>
       <Route exact path="/">
-        <Home />
+        {/* <Home /> */}
+        Home
       </Route>
       <Route path="/users">
-        <Users />
+        {/* <Users /> */}
+        Users
       </Route>
-      <Route path="/about">
-        <About />
-      </Route>
+      <Route path="/about">About</Route>
     </div>
   );
 }
